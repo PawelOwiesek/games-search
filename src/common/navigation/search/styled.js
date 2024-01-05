7cdbed4d89e1115d82e5bf35d8b65c9bbc42a62d
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-05 21:32:23 +0100

    search component styles

---
+++ src/common/navigation/search/styled.js
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+export const Input = styled.input`
+  min-width: 432px;
+  height: 48px;
+  border-radius: 33px;
+  border: 1px solid #131313;
+  background: #f4f4f4;
+  padding-left: 64px;
+  background-repeat: no-repeat;
+  background-position: 24px center;
+  background-size: 24px 24px;
+  margin-right: 200px;
+`;
