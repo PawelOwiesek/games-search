9232ad1931ce7f17c2204487e9196640faf7e36e
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-05 19:24:16 +0100

    render list of games and add pagination interaction

--- src/features/gamesPage/index.js
+++ src/features/gamesPage/index.js
@@ -1,8 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { fetchData } from "../fetchApiData";
+import Pagination from "../../common/pagination";
+
 const GamesPage = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [games, setGames] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
+
+  useEffect(() => {
+    const fetchDataFromApi = async () => {
+      try {
+        const data = await fetchData({ currentPage });
+        setGames(data.results);
+        setTotalPages(data.count);
+      } catch (error) {
+        console.error("Data download error:", error);
+      }
+    };
+
+    fetchDataFromApi();
+  }, [currentPage]);
+
   return (
-    <div>
-      <h1>hello world😃</h1>
-    </div>
+    <>
+      <div>
+        <ul>
+          {games.length > 0 ? (
+            games.map((game) => <li key={game.id}>{game.name}</li>)
+          ) : (
+            <li>No games available</li>
+          )}
+        </ul>
+      </div>{" "}
+      <Pagination
+        setCurrentPage={setCurrentPage}
+        totalPages={totalPages}
+        currentPage={currentPage}
+      />
+    </>
   );
 };
 
