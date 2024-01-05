f9f435e8142d00831ed63793d47c581a2adf10b2
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-05 21:31:57 +0100

    created Search component

---
+++ src/common/navigation/search/index.js
@@ -0,0 +1,11 @@
+import { Input } from "./styled";
+
+const Search = () => {
+  return (
+    <div>
+      <Input type="text" placeholder="Search for Your favorite game" />
+    </div>
+  );
+};
+
+export default Search;
