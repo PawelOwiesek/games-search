bb92159cdecfd1a5b84cbd64efe3e03b4fda0c6b
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-05 21:31:30 +0100

    styles for navigation

---
+++ src/common/navigation/styled.js
@@ -0,0 +1,24 @@
+import { styled } from "styled-components";
+import { ReactComponent as logo } from "../images/logo.svg";
+import { NavLink } from "react-router-dom";
+
+export const Container = styled.div`
+  background-color: #d7d1d1;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Logo = styled(logo)`
+  cursor: pointer;
+  fill: #008080;
+`;
+
+export const StyledLink = styled(NavLink)`
+  text-decoration-line: none;
+  cursor: pointer;
+  font-size: 25px;
+  color: #1a1a1a;
+  font-weight: 700;
+  margin-right: 300px;
+`;
