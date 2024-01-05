1f17bc36bdc5d3962ebbbc5c50331a058107322d
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-05 18:36:01 +0100

    initialized routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import {
+  Routes,
+  Route,
+  Navigate,
+  HashRouter as Router,
+} from "react-router-dom";
+import Navigation from "./common/navigation";
+import GamesPage from "./features/gamesPage";
+
 function App() {
   return (
-    <div>
-      <h1>Hello world😃</h1>
-    </div>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<GamesPage />} />
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
 
