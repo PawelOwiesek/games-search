2990293552a8d350dd79d4970b114ac7fd41ebcf
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-06 18:11:43 +0100

    wrapped App in store provider

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
 import { theme } from "./theme";
+import store from "./store.js";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />{" "}
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />{" "}
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
