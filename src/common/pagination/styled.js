20ee0ebae83d3b08e0bfbb717541e3b60446a753
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-06 13:26:34 +0100

    created styled Button for pagination

---
+++ src/common/pagination/styled.js
@@ -0,0 +1,12 @@
+import { styled } from "styled-components";
+
+export const Button = styled.button`
+  margin: 10px;
+  padding: 10px 15px;
+  border: none;
+  background-color: #b0bab0;
+  color: #1a1a1a;
+  border-radius: 25px;
+  font-size: 18px;
+  cursor: pointer;
+`;
