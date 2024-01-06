42ee877cfb62ab7f7a4c8d801b3929380b641ce3
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-06 13:36:48 +0100

    add changing color for disabled buttons

--- src/common/pagination/styled.js
+++ src/common/pagination/styled.js
@@ -6,7 +6,11 @@ export const Button = styled.button`
   border: none;
   background-color: #b0bab0;
   color: #1a1a1a;
-  border-radius: 25px;
+  border-radius: 5px;
   font-size: 18px;
   cursor: pointer;
+
+  &:disabled {
+    background-color: #7d7d7d;
+  }
 `;
