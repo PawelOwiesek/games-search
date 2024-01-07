a3bf445c742d8d0372fc36ea72d462daccd4488f
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-07 19:45:28 +0100

    styles for game tile component

---
+++ src/common/gameTile/styled.js
@@ -0,0 +1,39 @@
+import { styled } from "styled-components";
+
+export const List = styled.ul`
+  padding: 0;
+  list-style-type: none;
+`;
+
+export const Container = styled.div`
+  max-width: 1400px;
+  display: grid;
+  grid-template-columns: 1fr auto;
+  justify-content: center;
+  align-items: center;
+  background-color: #b0bab0;
+  margin-bottom: 100px;
+`;
+
+export const Title = styled.h1`
+  grid-column: 1/3;
+  justify-self: center;
+  font-size: 20px;
+  color: #1a1a1a;
+  font-weight: 700;
+`;
+
+export const Image = styled.img`
+  margin: 30px;
+  width: 600px;
+  height: 450px;
+`;
+
+export const Details = styled.div`
+  margin: 30px;
+  font-size: 20px;
+  color: #1a1a1a;
+  font-weight: 700;
+`;
+
+export const Description = styled.p``;
