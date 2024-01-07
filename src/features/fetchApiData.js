c78560127f152a835198a6bc69939565ca6a6ebf
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-07 22:05:32 +0100

    add fetch game description function and fetch video

--- src/features/fetchApiData.js
+++ src/features/fetchApiData.js
@@ -15,3 +15,27 @@ export const fetchData = async ({ currentPage }) => {
     console.error("Data download error:", error);
   }
 };
+
+export const fetchDescription = async ({ id }) => {
+  try {
+    const response = await axios.get(
+      `https://api.rawg.io/api/games/${id}?key=${APIKEY}`
+    );
+    return { description: response.data.description };
+  } catch (error) {
+    console.error("Błąd pobierania opisu gry:", error);
+  }
+};
+
+export const fetchVideo = async ({ id }) => {
+  try {
+    const response = await axios.get(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(
+        `${id} gameplay trailer`
+      )}&type=video&key=AIzaSyA5zv7QwmvMJqjSMoTjmkCIe5XCfumCtEk`
+    );
+    return { video: response.data };
+  } catch (error) {
+    console.error("Error downloading YouTube video:", error);
+  }
+};
