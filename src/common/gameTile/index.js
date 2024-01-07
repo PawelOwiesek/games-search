79d9e28561a3353e36fe62031757c1986fd5576f
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-07 22:37:11 +0100

    add part of game description to tile

--- src/common/gameTile/index.js
+++ src/common/gameTile/index.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { fetchDescription } from "../../features/fetchApiData";
 import PlatformTile from "../platformTile";
 import {
   Container,
@@ -10,6 +12,18 @@ import {
 } from "./styled";
 
 const GameTile = ({ game, id, name, image }) => {
+  const [description, setDescription] = useState("");
+
+  const fetchGameDescription = async () => {
+    try {
+      const data = await fetchDescription({ id });
+      setDescription(data.description);
+    } catch (error) {
+      console.error("Error downloading game description:", error);
+    }
+  };
+  fetchGameDescription();
+
   return (
     <List>
       <Item key={id}>
@@ -18,18 +32,8 @@ const GameTile = ({ game, id, name, image }) => {
           <Details>
             <PlatformTile game={game} />
             <Description>
-              Contrary to popular belief, Lorem Ipsum is not simply random text.
-              It has roots in a piece of classical Latin literature from 45 BC,
-              making it over 2000 years old. Richard McClintock, a Latin
-              professor at Hampden-Sydney College in Virginia, looked up one of
-              the more obscure Latin words, consectetur, from a Lorem Ipsum
-              passage, and going through the cites of the word in classical
-              literature, discovered the undoubtable source. Lorem Ipsum comes
-              from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et
-              Malorum" (The Extremes of Good and Evil) by Cicero, written in 45
-              BC. This book is a treatise on the theory of ethics, very popular
-              during the Renaissance. The first line of Lorem Ipsum, "Lorem
-              ipsum dolor sit amet..", comes from a line in section 1.10.32.
+              {" "}
+              {description.replace(/<[^>]*>/g, "").slice(0, 350)} ...
             </Description>{" "}
           </Details>
         </Container>
