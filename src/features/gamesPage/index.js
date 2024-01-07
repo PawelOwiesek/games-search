446fe3602c92b6606680c71189b5966c5abfbb09
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-07 19:47:44 +0100

    used tiles to render view

--- src/features/gamesPage/index.js
+++ src/features/gamesPage/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { fetchData } from "../fetchApiData";
 import Pagination from "../../common/pagination";
+import GameTile from "../../common/gameTile";
 
 const GamesPage = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -23,82 +24,18 @@ const GamesPage = () => {
 
   return (
     <>
-      <div>
-        <ul style={{ padding: "0", listStyleType: "none" }}>
-          {games.length > 0 ? (
-            games.map((game) => (
-              <li key={game.id}>
-                <div
-                  style={{
-                    maxWidth: "1400px",
-                    display: "grid",
-                    gridTemplateColumns: "1fr auto",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    backgroundColor: "#b0bab0",
-                    marginBottom: "100px",
-                  }}
-                >
-                  <p
-                    style={{
-                      gridColumn: "1/3",
-                      justifySelf: "center",
-                      fontSize: "20px",
-                      color: "#1a1a1a",
-                      fontWeight: "700",
-                    }}
-                  >
-                    {game.name}
-                  </p>
-                  <p style={{ marginLeft: "30px" }}>
-                    {" "}
-                    <img
-                      key={game.id}
-                      width="600px"
-                      height="450px"
-                      src={game.background_image}
-                      alt={`Screenshot `}
-                    />{" "}
-                  </p>
-                  <div
-                    style={{
-                      margin: "30px",
-                      fontSize: "20px",
-                      color: "#1a1a1a",
-                      fontWeight: "700",
-                    }}
-                  >
-                    <p>Platforms:</p>
-                    <ul>
-                      {game.platforms.map((platform) => (
-                        <li key={platform.platform.id}>
-                          {platform.platform.name}
-                        </li>
-                      ))}
-                    </ul>
-                    Contrary to popular belief, Lorem Ipsum is not simply random
-                    text. It has roots in a piece of classical Latin literature
-                    from 45 BC, making it over 2000 years old. Richard
-                    McClintock, a Latin professor at Hampden-Sydney College in
-                    Virginia, looked up one of the more obscure Latin words,
-                    consectetur, from a Lorem Ipsum passage, and going through
-                    the cites of the word in classical literature, discovered
-                    the undoubtable source. Lorem Ipsum comes from sections
-                    1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The
-                    Extremes of Good and Evil) by Cicero, written in 45 BC. This
-                    book is a treatise on the theory of ethics, very popular
-                    during the Renaissance. The first line of Lorem Ipsum,
-                    "Lorem ipsum dolor sit amet..", comes from a line in section
-                    1.10.32.
-                  </div>
-                </div>
-              </li>
-            ))
-          ) : (
-            <li>No games available</li>
-          )}
-        </ul>
-      </div>{" "}
+      {games.length > 0 ? (
+        games.map((game) => (
+          <GameTile
+            id={game.id}
+            game={game}
+            name={game.name}
+            image={game.background_image}
+          />
+        ))
+      ) : (
+        <li>No games available</li>
+      )}
       <Pagination
         setCurrentPage={setCurrentPage}
         totalPages={totalPages}
