15bef5043a5b9b6be54de71f39a89e8790c54242
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-08 12:12:43 +0100

    changed page background color

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -13,7 +13,7 @@ html {
 body {
     max-width:1400px;
     margin:0 auto;
-    background-color: ${({ theme }) => theme.color.whisper};
+    background-color: ${({ theme }) => theme.color.black};
     font-family: "Roboto", sans-serif;
     word-break: break-word;
     overflow-y: scroll;
