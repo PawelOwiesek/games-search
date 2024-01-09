83bdffb8b764a2d5016d2e3bc9fc29796e3ac571
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-09 10:08:32 +0100

    add icons for theme button

---
+++ src/common/images/index.js
@@ -0,0 +1,2 @@
+export const nightImage = `url("https://i.postimg.cc/T2q7G0ry/dark-night-half-moon-vector-260nw-1567237021.webp")`;
+export const dayImage = `url("https://i.postimg.cc/yxMYLKvd/vector-sky-and-sun-background.jpg")`;
