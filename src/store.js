aeca1b73d08ea1d0aa7a79486c682d7cc77ca7dc
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-09 10:04:36 +0100

    add themeReducer  and middleware

--- src/store.js
+++ src/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
 import gamesReducer from "./features/gamesPage/gamesSlice";
+import themeReducer from "./common/ThemeSlider/themeSlice";
+import { rootSaga } from "./rootSaga";
 
-export default configureStore({
+const sagaMiddleware = createSagaMiddleware();
+const store = configureStore({
   reducer: {
     games: gamesReducer,
+    theme: themeReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware),
 });
+sagaMiddleware.run(rootSaga);
+export default store;
