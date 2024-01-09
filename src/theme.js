f9b5bc40117bb5b9220683659e3a79483070d65b
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-09 10:05:24 +0100

    add colors, theme dark , theme light with breakpoints

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,55 @@
-export const theme = {
-  color: { whisper: "#F5F5FA", black: "#000" },
+import { nightImage, dayImage } from "./common/images";
+const colors = {
+  whisper: "#F5F5FA",
+  black: "#1a1a1a",
+  white: "#FFFFFF",
+  coffie: "#d7d1d1",
+  lemon: "#efe116",
+  spring: "#b0bab0",
+  boulder: "#7d7d7d",
+  teal: "#008080",
+};
+
+export const darkTheme = {
+  mainColor: colors.black,
+  primaryColor: colors.coffie,
+  logo: colors.teal,
+  background: colors.spring,
+  disabledColor: colors.boulder,
+  hoverColor: colors.lemon,
+  alternativeBackground: colors.white,
+  themeButton: {
+    backgroundImage: nightImage,
+  },
+
+  breakpoint: {
+    desktopMax: "1368px",
+    desktop: "1200px",
+    ipod: "1024px",
+    large: "767px",
+    medium: "450px",
+    small: "320px",
+  },
+};
+
+export const lightTheme = {
+  mainColor: colors.white,
+  primaryColor: colors.boulder,
+  logo: colors.lemon,
+  background: colors.black,
+  disabledColor: colors.boulder,
+  hoverColor: colors.lemon,
+  alternativeBackground: colors.spring,
+  themeButton: {
+    backgroundImage: dayImage,
+  },
+
+  breakpoint: {
+    desktopMax: "1368px",
+    desktop: "1200px",
+    ipod: "1024px",
+    large: "767px",
+    medium: "450px",
+    small: "320px",
+  },
 };
