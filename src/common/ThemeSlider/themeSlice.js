3a501fce3294c71b8b69a2229ac3990a3bcc5bfc
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-09 10:12:45 +0100

    add slice for theme interacions

---
+++ src/common/ThemeSlider/themeSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { saveThemeInLocalStorage } from "./themeLocalStorage";
+import { themeFromLocalStorage } from "./themeLocalStorage";
+
+const themeSlice = createSlice({
+  name: "theme",
+  initialState: {
+    isTheme: themeFromLocalStorage(),
+  },
+  reducers: {
+    toggleTheme: (state) => {
+      state.isTheme = !state.isTheme;
+      saveThemeInLocalStorage(state.isTheme);
+    },
+  },
+});
+
+export const selectThemeState = (state) => state.theme;
+export const { toggleTheme } = themeSlice.actions;
+export const selectLightTheme = (state) => selectThemeState(state).isTheme;
+export default themeSlice.reducer;
+console.log(selectThemeState({ theme: { isTheme: true } }));
