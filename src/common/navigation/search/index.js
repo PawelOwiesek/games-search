bf18f7a67c86f97d19ba2add074950294ba61d9d
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-09 12:08:57 +0100

    text of placeholder depends on window width

--- src/common/navigation/search/index.js
+++ src/common/navigation/search/index.js
@@ -1,9 +1,11 @@
 import { Input } from "./styled";
 
 const Search = () => {
+  const isMobile = window.innerWidth <= 1024;
+  const placeholder = isMobile ? "Find game" : "Search for Your favorite game";
   return (
     <div>
-      <Input type="text" placeholder="Search for Your favorite game" />
+      <Input type="text" placeholder={placeholder} />
     </div>
   );
 };
