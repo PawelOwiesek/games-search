9962de271976a30a2af59a07842ca637485656c2
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-09 10:03:01 +0100

    moved theme provider to app.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./GlobalStyle";
-import { theme } from "./theme";
 import store from "./store.js";
 import { Provider } from "react-redux";
 
@@ -12,10 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />{" "}
-      </ThemeProvider>
+      <App />{" "}
     </Provider>
   </React.StrictMode>
 );
