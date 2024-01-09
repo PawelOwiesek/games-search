3da47f082d64ac8d7a843f9c6a8e4203ea826599
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-09 10:01:51 +0100

    add themeProvider and global styles

--- src/App.js
+++ src/App.js
@@ -7,17 +7,27 @@ import {
 import Navigation from "./common/navigation";
 import GamesPage from "./features/gamesPage";
 import Description from "./features/description";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./GlobalStyle";
+import { useSelector } from "react-redux";
+import { selectLightTheme } from "./common/ThemeSlider/themeSlice";
+import { darkTheme, lightTheme } from "./theme";
 
 function App() {
+  const isLightTheme = useSelector(selectLightTheme);
+
   return (
-    <Router>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<GamesPage />} />
-        <Route path="/Description/:id" element={<Description />} />
-        <Route path="/*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={isLightTheme ? darkTheme : lightTheme}>
+      <GlobalStyle />
+      <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<GamesPage />} />
+          <Route path="/Description/:id" element={<Description />} />
+          <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
