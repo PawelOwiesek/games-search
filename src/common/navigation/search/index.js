8237a5441e020843521d6053d620ca1080aafe0d
Pawel Owiesek <Owiesek.p@gmail.com>
2024-01-10 11:40:32 +0100

    search component initialization(not ready yet)

--- src/common/navigation/search/index.js
+++ src/common/navigation/search/index.js
@@ -1,11 +1,33 @@
 import { Input } from "./styled";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Search = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const query = new URLSearchParams(location.search).get("search");
+
+  const onInputChange = ({ target }) => {
+    const searchParams = new URLSearchParams(location.search);
+
+    if (target.value.trim() === "") {
+      searchParams.delete("search");
+    } else {
+      searchParams.set("search", target.value);
+    }
+
+    navigate(`${location.pathname}?${searchParams.toString()}`);
+  };
+
   const isMobile = window.innerWidth <= 1024;
   const placeholder = isMobile ? "Find game" : "Search for Your favorite game";
+
   return (
     <div>
-      <Input type="text" placeholder={placeholder} />
+      <Input
+        value={query || ""}
+        onChange={onInputChange}
+        placeholder={placeholder}
+      />
     </div>
   );
 };
